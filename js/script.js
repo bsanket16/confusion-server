81e8b5d8974175e746fa16a2f9387e1c1109df80
sankt <bsankt16@gmail.com>
2020-06-17 20:59:51 +0530

    npm scripts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+
+$("#show").click(function () {
+    $('#reserve').modal('show')
+})
+
+$("#login").click(function () {
+    $('#loginModal').modal('show')
+})
